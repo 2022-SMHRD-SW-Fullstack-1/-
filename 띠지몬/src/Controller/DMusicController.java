330ba52fc0800a5780b9a567b4fef45c85a4a6c7
smhrd <smhrd@DESKTOP-L21GC4V>
2022-09-06 20:46:33 +0900

    아스키 추가

--- 띠지몬/src/Controller/DMusicController.java
+++ 띠지몬/src/Controller/DMusicController.java
@@ -20,92 +20,37 @@ public class DMusicController {
 	}
 	
 	public void opening() {
-		if(mp3.isPlaying()) {
-			mp3.stop();
+		try {			
+			if(mp3.isPlaying()) {
+				mp3.stop();
+			}
+			mp3.play(mList.get(0).getMusicPath());
+		}catch(Exception e) {
 		}
-		mp3.play(mList.get(0).getMusicPath());
 	}
 	
 	public void ending() {
-		if(mp3.isPlaying()) {
-			mp3.stop();
+		try {			
+			if(mp3.isPlaying()) {
+				mp3.stop();
+			}
+			mp3.play(mList.get(1).getMusicPath());
+		}catch(Exception e) {
 		}
-		mp3.play(mList.get(1).getMusicPath());
 	}
 	
 	public void battle() {
-		if(mp3.isPlaying()) {
-			mp3.stop();
+		try {			
+			if(mp3.isPlaying()) {
+				mp3.stop();
+			}
+			mp3.play(mList.get(2).getMusicPath());
+		}catch(Exception e) {
 		}
-		mp3.play(mList.get(2).getMusicPath());
 	}
-	
-//	
-//	
-//	public void gameStart(File Sound) {
-//	try {		
-//		Clip clip = AudioSystem.getClip();
-//		AudioInputStream ais = AudioSystem.getAudioInputStream(Sound);
-//		clip.open(ais);
-//		clip.start();
-//	}
-//	catch (Exception e) {
-//		System.out.println("음악이 재생되지 않습니다.");
-//		e.printStackTrace();
-//	}
-//}
-//	
-//	public void dStop(File Sound) {
-//	
-//		try {
-//			Clip clip = AudioSystem.getClip();
-//			AudioInputStream ais = AudioSystem.getAudioInputStream(Sound);
-//			clip.open(ais);
-//			clip.stop();
-//			clip.close();
-//			
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-	
-
-//	public void play() {
-//		// 노래 재생을 위한 메소드
-//		// 호출시 mList에 있는 노래를 play
-//		if(mp3.isPlaying()) {
-//			mp3.stop();
-//		}
-//		mp3.play(mList.get(index).getMusicPath());
-//	}
 	
 	public void stop() {
 		mp3.stop();
 	}
 	
-	public void next() {
-		if(mp3.isPlaying()) {
-			mp3.stop();
-		}
-		
-		// index 번호가 3일 때 다시 0으로 돌아갈 수 있는 작업 진행
-		if(index<mList.size()-1) {
-			index++;	
-		}else { //마지막 인덱스에서 다시 다음버튼을 눌렀을 때
-			index = 0;
-		}
-			mp3.play(mList.get(index).getMusicPath());			
-	}
-	
-	public void pre() {
-		if(mp3.isPlaying()) {
-			mp3.stop();
-		}
-		if(index > 0) {
-			index--;
-		}else { //마지막 인덱스에서 다시 다음부턴을 눌렀을 때
-			index = mList.size()-1;
-		}
-			mp3.play(mList.get(index).getMusicPath());
-	}
 }
